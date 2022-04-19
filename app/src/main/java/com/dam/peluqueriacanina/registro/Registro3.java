6e917b56f2aa80e313a8d48b77c7cf97a32eddc5
Byrevenge3r <car.alleway@gmail.com>
2022-04-19 22:32:17 +0200

    Fusion (trabajo fernando y rodrigo) Remodelacion del activity_main.xml Creacion de todos los registros (activity_registro1.xml, activity_registro2.xml, activity_registro3.xml) (bamoh lo eslai del registro farta e reto jabe uno ma uno mamasita)

---
+++ app/src/main/java/com/dam/peluqueriacanina/registro/Registro3.java
@@ -0,0 +1,16 @@
+package com.dam.peluqueriacanina.registro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dam.peluqueriacanina.R;
+
+public class Registro3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro3);
+    }
+}
