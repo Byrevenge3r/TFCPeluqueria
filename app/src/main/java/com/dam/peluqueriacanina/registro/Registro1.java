c7aa409500136eb33983fa71f1aee9231dfca755
perico9102 <ferpascan@gmail.com>
2022-04-20 21:54:27 +0200

    Terminado Registro1 y Registro2 con sus animaciones

--- app/src/main/java/com/dam/peluqueriacanina/registro/Registro1.java
+++ app/src/main/java/com/dam/peluqueriacanina/registro/Registro1.java
@@ -2,15 +2,35 @@ package com.dam.peluqueriacanina.registro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.dam.peluqueriacanina.R;
 
-public class Registro1 extends AppCompatActivity {
+public class Registro1 extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSiguienteRegUno;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro1);
+
+        btnSiguienteRegUno = findViewById(R.id.btnSiguienteRegUno);
+
+        btnSiguienteRegUno.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(btnSiguienteRegUno)) {
+            i = new Intent(this, Registro2.class);
+            startActivity(i);
+
+            overridePendingTransition(R.anim.animacion_derecha_izquierda,R.anim.animacion_izquierda_izquierda);
+        }
     }
 }
--- app/src/main/java/com/dam/peluqueriacanina/registro/Registro2.java
+++ app/src/main/java/com/dam/peluqueriacanina/registro/Registro2.java
@@ -2,15 +2,52 @@ package com.dam.peluqueriacanina.registro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.dam.peluqueriacanina.R;
 
-public class Registro2 extends AppCompatActivity {
+public class Registro2 extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAtrasRegDos, btnSiguienteRegDos;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro2);
+
+        btnAtrasRegDos = findViewById(R.id.btnAtrasRegDos);
+        btnSiguienteRegDos = findViewById(R.id.btnSiguienteRegDos);
+
+        btnAtrasRegDos.setOnClickListener(this);
+        btnSiguienteRegDos.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+       if (v.equals(btnSiguienteRegDos)) {
+            i = new Intent(this, Registro3.class);
+            startActivity(i);
+
+            overridePendingTransition(R.anim.animacion_izquierda_izquierda,R.anim.animacion_derecha_izquierda);
+        } else if (v.equals(btnAtrasRegDos)) {
+           i = new Intent(this, Registro1.class);
+           startActivity(i);
+
+           overridePendingTransition(R.anim.animacion_derecha_derecha,R.anim.animacion_izquierda_derecha);
+       }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        i = new Intent(this, Registro1.class);
+        startActivity(i);
+
+        overridePendingTransition(R.anim.animacion_derecha_derecha,R.anim.animacion_izquierda_derecha);
     }
 }
