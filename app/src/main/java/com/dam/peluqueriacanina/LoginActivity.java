4bdcf2268cf424729d88f0d77446bd6791c34d94
Byrevenge3r <car.alleway@gmail.com>
2022-04-21 21:36:55 +0200

    Registros 3 y 4

--- app/src/main/java/com/dam/peluqueriacanina/LoginActivity.java
+++ app/src/main/java/com/dam/peluqueriacanina/LoginActivity.java
@@ -2,13 +2,43 @@ package com.dam.peluqueriacanina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    FirebaseAuth fAuth;
+    FirebaseUser fUser;
+    FirebaseDatabase fdb;
+    DatabaseReference dbRef;
+    TextInputLayout etCorreo, etContrasenia;
+    Button btnAcc;
+    Button btnReg;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fAuth = FirebaseAuth.getInstance();
+        fUser = fAuth.getCurrentUser();
+
+        fdb = FirebaseDatabase.getInstance();
+        dbRef = fdb.getReference("usuarios");
+
+        etCorreo = findViewById(R.id.etCorreo);
+        etContrasenia = findViewById(R.id.etContrasenia);
+        //btnAcc = findViewById(R.id.btnAcceder);
+
+
     }
 }
--- app/src/main/java/com/dam/peluqueriacanina/registro/Registro3.java
+++ app/src/main/java/com/dam/peluqueriacanina/registro/Registro3.java
@@ -2,15 +2,53 @@ package com.dam.peluqueriacanina.registro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.dam.peluqueriacanina.R;
 
-public class Registro3 extends AppCompatActivity {
+public class Registro3 extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnAtrasRegTres, btnSiguienteRegTres;
+
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro3);
+
+        btnAtrasRegTres = findViewById(R.id.btnAtrasRegTres);
+        btnSiguienteRegTres = findViewById(R.id.btnSiguienteRegTres);
+
+        btnAtrasRegTres.setOnClickListener(this);
+        btnSiguienteRegTres.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(btnSiguienteRegTres)) {
+            i = new Intent(this, Registro4.class);
+            startActivity(i);
+
+            overridePendingTransition(R.anim.animacion_izquierda_izquierda,R.anim.animacion_derecha_izquierda);
+        } else if (v.equals(btnAtrasRegTres)) {
+            i = new Intent(this, Registro2.class);
+            startActivity(i);
+
+            overridePendingTransition(R.anim.animacion_derecha_derecha,R.anim.animacion_izquierda_derecha);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        i = new Intent(this, Registro2.class);
+        startActivity(i);
+
+        overridePendingTransition(R.anim.animacion_derecha_derecha,R.anim.animacion_izquierda_derecha);
     }
 }
