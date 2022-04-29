2d18389fff25fa8f047f042cca37ea26d51262b2
perico9102 <ferpascan@gmail.com>
2022-04-29 17:23:13 +0200

    webada de mapa

--- app/src/main/java/com/dam/peluqueriacanina/UbicacionTiempoRealActivity.java
+++ app/src/main/java/com/dam/peluqueriacanina/UbicacionTiempoRealActivity.java
@@ -1,43 +1,119 @@
 package com.dam.peluqueriacanina;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 
+import com.dam.peluqueriacanina.model.Mapa;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.dam.peluqueriacanina.databinding.ActivityUbicacionTiempoRealBinding;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UbicacionTiempoRealActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private ActivityUbicacionTiempoRealBinding binding;
+    GoogleMap mMap;
+    ActivityUbicacionTiempoRealBinding binding;
+    FirebaseDatabase fdb;
+    DatabaseReference dbr;
+    Marker marker;
+    CameraUpdate ubicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fdb = FirebaseDatabase.getInstance();
+        dbr = fdb.getReference();
+        setContentView(R.layout.activity_ubicacion_tiempo_real);
 
-        binding = ActivityUbicacionTiempoRealBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+    }
+
+    private void coorMapa() {
+        dbr.addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Mapa mp = snapshot.getValue(Mapa.class);
+                if (mp.getLatitud() != 0.0 && mp.getLongitud() != 0.0) {
+                    Double latitud = mp.getLatitud();
+                    Double longitud = mp.getLongitud();
+
+                    Log.e("ubicaa",latitud + " " + longitud);
+
+                if (marker != null) {
+                    marker.remove();
+                }
+
+                marker = mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.furgo_canina))
+                                    .position(new LatLng(latitud, longitud)));
+
+                ubicacion = CameraUpdateFactory.newLatLng(new LatLng(latitud,longitud));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitud,longitud), 13));
+                    countDownTimer();
+                }
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    private void countDownTimer() {
+        new CountDownTimer(  2000, 1000) {
+            public void onTick (long millisuntilFinished) {
+                onMapReady(mMap);
+            }
+
+            public void onFinish () {
+
+            }
+
+        }.start ();
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        coorMapa();
     }
 }
---
+++ app/src/main/java/com/dam/peluqueriacanina/model/Mapa.java
@@ -0,0 +1,25 @@
+package com.dam.peluqueriacanina.model;
+
+public class Mapa {
+    private double latitud;
+    private double longitud;
+
+    public Mapa() {
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+}
