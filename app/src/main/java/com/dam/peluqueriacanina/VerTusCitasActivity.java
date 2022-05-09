7e2e5d7c86e132ce3af1875aea9cdfc8508819e1
perico9102 <ferpascan@gmail.com>
2022-05-09 21:17:51 +0200

    rodri chupamela 2

--- app/src/main/java/com/dam/peluqueriacanina/VerTusCitasActivity.java
+++ app/src/main/java/com/dam/peluqueriacanina/VerTusCitasActivity.java
@@ -25,9 +25,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
-public class VerTusCitasActivity extends AppCompatActivity {
+public class VerTusCitasActivity extends AppCompatActivity implements View.OnClickListener {
 
     RecyclerView rv;
     LinearLayoutManager llm;
@@ -44,6 +48,9 @@ public class VerTusCitasActivity extends AppCompatActivity {
     TextView tvNoHayCitasVTC;
     Button btnCancelarCita;
     AnimalPeluAdapter adapter;
+    SimpleDateFormat formatter;
+    Date fecha;
+    View vista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +58,7 @@ public class VerTusCitasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ver_tus_citas);
 
         fdb = FirebaseDatabase.getInstance();
-        dbr = fdb.getReference();
+        dbr = fdb.getReference("coche/reservas");
 
         dbTusCitas = TusCitasDB.getDatabase(this);
         daoTusCitas = dbTusCitas.citaDao();
@@ -68,83 +75,95 @@ public class VerTusCitasActivity extends AppCompatActivity {
 
         rv.setLayoutManager(llm);
 
-        if (!daoTusCitas.sacarTodo().isEmpty()) {
-            adapter = new AnimalPeluAdapter((ArrayList<TusCitas>) daoTusCitas.sacarTodo());
-            adapter.setListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        formatter = new SimpleDateFormat("dd/MM/yyyy");
+        fecha = new Date();
 
-                    tusCitas = (daoTusCitas.sacarTodo().get(rv.getChildAdapterPosition(v)));
 
-                    int mes = Integer.parseInt(tusCitas.getFecha().substring(1,2));
 
-                    switch (mes) {
-                        case 1:
-                            mesN = "enero";
-                            break;
-                        case 2:
-                            mesN = "febrero";
-                            break;
-                        case 3:
-                            mesN = "marzo";
-                            break;
-                        case 4:
-                            mesN = "abril";
-                            break;
-                        case 5:
-                            mesN = "mayo";
-                            break;
-                        case 6:
-                            mesN = "junio";
-                            break;
-                        case 7:
-                            mesN = "julio";
-                            break;
-                        case 8:
-                            mesN = "agosto";
-                            break;
-                        case 9:
-                            mesN = "septiembre";
-                            break;
-                        case 10:
-                            mesN = "octubre";
-                            break;
-                        case 11:
-                            mesN = "noviembre";
-                            break;
-                        case 12:
-                            mesN = "diciembre";
-                            break;
-                    }
+        btnCancelarCita.setOnClickListener(this);
+        vista = new View(this);
 
-                    Query q = dbr.child("coche/reservas/" + mes).orderByChild("fecha").equalTo(tusCitas.getFecha());
-                    q.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            for (DataSnapshot appleSnapshot : dataSnapshot.getChildren()) {
-                                appleSnapshot.getRef().removeValue();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            Log.e("erroraa", "onCancelled", databaseError.toException());
-                        }
-                    });
-
-                    citas = citasDao.sacarCitasRuta(tusCitas.getRuta());
-                    citasDao.delete(citas);
-                    daoTusCitas.delete(tusCitas);
-
-                }
-            });
-
-        } else {
+        adapter = new AnimalPeluAdapter((ArrayList<TusCitas>) daoTusCitas.sacarTodo());
+        if (daoTusCitas.sacarTodo().isEmpty()) {
             tvNoHayCitasVTC.setVisibility(View.VISIBLE);
         }
+        adapter.setListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnCancelarCita.setVisibility(View.VISIBLE);
+                vista = v;
+            }
+        });
 
         rv.setAdapter(adapter);
 
     }
 
+    @Override
+    public void onClick(View view) {
+        tusCitas = daoTusCitas.sacarTodo().get(rv.getChildAdapterPosition(vista));
+        try {
+            fecha = formatter.parse(tusCitas.getFecha());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(fecha);
+        int mesActual = cal.get(Calendar.MONTH)+1;
+
+        switch (mesActual) {
+            case 1:
+                mesN = "enero";
+                break;
+            case 2:
+                mesN = "febrero";
+                break;
+            case 3:
+                mesN = "marzo";
+                break;
+            case 4:
+                mesN = "abril";
+                break;
+            case 5:
+                mesN = "mayo";
+                break;
+            case 6:
+                mesN = "junio";
+                break;
+            case 7:
+                mesN = "julio";
+                break;
+            case 8:
+                mesN = "agosto";
+                break;
+            case 9:
+                mesN = "septiembre";
+                break;
+            case 10:
+                mesN = "octubre";
+                break;
+            case 11:
+                mesN = "noviembre";
+                break;
+            case 12:
+                mesN = "diciembre";
+                break;
+        }
+
+        Query q = dbr.child(mesN).orderByChild("fecha").equalTo(tusCitas.getFecha());
+        q.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot appleSnapshot : dataSnapshot.getChildren()) {
+                    appleSnapshot.getRef().removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("erroraa", "onCancelled", databaseError.toException());
+            }
+        });
+    }
 }
