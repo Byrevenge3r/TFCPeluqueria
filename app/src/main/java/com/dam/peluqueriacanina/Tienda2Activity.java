c9f695ced3de6a7f4475c267e6bffe0919800683
samuel <samuel_romero@outlook.com>
2022-05-10 21:03:16 +0200

    video 2/4 Samu 10/05/22

---
+++ app/src/main/java/com/dam/peluqueriacanina/Tienda2Activity.java
@@ -0,0 +1,14 @@
+package com.dam.peluqueriacanina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tienda2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tienda2);
+    }
+}
