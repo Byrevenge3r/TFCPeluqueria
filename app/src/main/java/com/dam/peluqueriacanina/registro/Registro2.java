6d658e14531a95c59611fa886a0d67eebb1dba06
perico9102 <ferpascan@gmail.com>
2022-06-14 23:02:17 +0200

    asdf

--- app/src/main/java/com/dam/peluqueriacanina/registro/Registro2.java
+++ app/src/main/java/com/dam/peluqueriacanina/registro/Registro2.java
@@ -7,14 +7,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dam.peluqueriacanina.R;
+import com.dam.peluqueriacanina.mainActivity.MainActivity;
+import com.dam.peluqueriacanina.model.User;
 import com.dam.peluqueriacanina.utils.MiApplication;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Registro2 extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,6 +41,8 @@ public class Registro2 extends AppCompatActivity implements View.OnClickListener
     DatabaseReference dbRef;
     FirebaseDatabase fb;
 
+    ArrayList<User> listaUser = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +50,7 @@ public class Registro2 extends AppCompatActivity implements View.OnClickListener
 
         fb = FirebaseDatabase.getInstance();
         fAuth = FirebaseAuth.getInstance();
-        dbRef = fb.getReference();
+        dbRef = fb.getReference("usuarios");
 
         etCorreo = findViewById(R.id.etEmailRegDos);
         etConfCorreo = findViewById(R.id.etConfirmarEmailRegDos);
@@ -61,43 +74,63 @@ public class Registro2 extends AppCompatActivity implements View.OnClickListener
         String confcontra = etConfContra.getText().toString().trim();
         String direccion = etdireccion.getText().toString().trim();
 
+
         if (v.equals(btnSiguienteRegDos)) {
-            if (TextUtils.isEmpty(correo)) {
-                etCorreo.setError("Introduzca un correo");
-            } else if (!comprobarArroba(correo)) {
-                etCorreo.setError("El correo debe contener un @");
-            }
+            Query q = dbRef.orderByChild("correo").equalTo(correo);
+            q.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (!snapshot.exists()) {
+                        if (!TextUtils.isEmpty(correo)) {
+                            if (!comprobarArroba(correo)) {
+                                etCorreo.setError("El correo debe contener un @");
+                            }
+                        } else {
+                            etCorreo.setError("Introduzca un correo");
+                        }
 
-            if (TextUtils.isEmpty(confcorreo)) {
-                 etConfCorreo.setError("Introduzca una confirmacion de correo");
-            } else if (TextUtils.isEmpty(contra)) {
-                Snackbar.make(v, R.string.contrasenia_vacia, Snackbar.LENGTH_LONG).show();
-            } else if (TextUtils.isEmpty(confcontra)) {
-                Snackbar.make(v, R.string.contrasenia_vacia, Snackbar.LENGTH_LONG).show();
-            } else if (contra.length() < 8) {
-                Snackbar.make(v, R.string.longitud_contra, Snackbar.LENGTH_LONG).show();
-            } else if (confcontra.length() < 8) {
-                Snackbar.make(v, R.string.longitud_contra, Snackbar.LENGTH_LONG).show();
-            } else if (!correo.equals(confcorreo)) {
-                Snackbar.make(v, R.string.correo_coincide, Snackbar.LENGTH_LONG).show();
-            } else if (!contra.equals(confcontra)) {
-                Snackbar.make(v, R.string.contra_coincide, Snackbar.LENGTH_LONG).show();
-            } else if (!hasUpperCase(contra)) {
-                Snackbar.make(v, R.string.contra_minuscula, Snackbar.LENGTH_LONG).show();
-            } else if (!hasSymbol(contra)) {
-                Snackbar.make(v, R.string.simbolo_contra, Snackbar.LENGTH_LONG).show();
-            } else if (TextUtils.isEmpty(direccion)) {
-                Snackbar.make(v, R.string.direccion_vacia, Snackbar.LENGTH_LONG).show();
-            } else {
-                ((MiApplication) getApplicationContext()).setCorreo(correo);
-                ((MiApplication) getApplicationContext()).setContrasenia(contra);
-                ((MiApplication) getApplicationContext()).setDireccion(direccion);
+                        if (TextUtils.isEmpty(confcorreo)) {
+                            etConfCorreo.setError("Introduzca una confirmacion de correo");
+                        } else if (TextUtils.isEmpty(contra)) {
+                            Snackbar.make(v, R.string.contrasenia_vacia, Snackbar.LENGTH_LONG).show();
+                        } else if (TextUtils.isEmpty(confcontra)) {
+                            Snackbar.make(v, R.string.contrasenia_vacia, Snackbar.LENGTH_LONG).show();
+                        } else if (contra.length() < 8) {
+                            Snackbar.make(v, R.string.longitud_contra, Snackbar.LENGTH_LONG).show();
+                        } else if (confcontra.length() < 8) {
+                            Snackbar.make(v, R.string.longitud_contra, Snackbar.LENGTH_LONG).show();
+                        } else if (!correo.equals(confcorreo)) {
+                            Snackbar.make(v, R.string.correo_coincide, Snackbar.LENGTH_LONG).show();
+                        } else if (!contra.equals(confcontra)) {
+                            Snackbar.make(v, R.string.contra_coincide, Snackbar.LENGTH_LONG).show();
+                        } else if (!hasUpperCase(contra)) {
+                            Snackbar.make(v, R.string.contra_minuscula, Snackbar.LENGTH_LONG).show();
+                        } else if (!hasSymbol(contra)) {
+                            Snackbar.make(v, R.string.simbolo_contra, Snackbar.LENGTH_LONG).show();
+                        } else if (TextUtils.isEmpty(direccion)) {
+                            Snackbar.make(v, R.string.direccion_vacia, Snackbar.LENGTH_LONG).show();
+                        } else {
+                            ((MiApplication) getApplicationContext()).setCorreo(correo);
+                            ((MiApplication) getApplicationContext()).setContrasenia(contra);
+                            ((MiApplication) getApplicationContext()).setDireccion(direccion);
 
-                i = new Intent(this, Registro3.class);
-                startActivity(i);
+                            i = new Intent(Registro2.this, Registro3.class);
+                            startActivity(i);
 
-                overridePendingTransition(R.anim.animacion_derecha_izquierda, R.anim.animacion_izquierda_izquierda);
-            }
+                            overridePendingTransition(R.anim.animacion_derecha_izquierda, R.anim.animacion_izquierda_izquierda);
+                        }
+                    } else {
+                        etCorreo.setError("Este correo ya existe");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+
 
         } else if (v.equals(btnAtrasRegDos)) {
             i = new Intent(this, Registro1.class);
@@ -105,6 +138,11 @@ public class Registro2 extends AppCompatActivity implements View.OnClickListener
 
             overridePendingTransition(R.anim.animacion_derecha_derecha, R.anim.animacion_izquierda_derecha);
         }
+
+
+    }
+
+    private void buscarUsuario(String correo) {
 
 
     }
